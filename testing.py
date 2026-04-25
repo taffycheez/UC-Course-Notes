a186666f1a4fa06926bb2e6b8e2fb1dae6b36df6
Varya Kern <varya.kern@gmail.com>
2026-04-25 22:45:42 +1200

    notes on recursion and divide & conquer

--- testing.py
+++ testing.py
@@ -1,112 +1,17 @@
-class Vec:
-    """A simple vector in 2D. Can also be used as a position vector from
-       origin to define points.
-    """
-    
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+def num_rushes(slope_height, rush_height_gain, back_sliding):
+    rushes = [0]
+    rush(rushes, 0, slope_height, rush_height_gain, back_sliding)
+    return rushes[0]
 
-    def __add__(self, other):
-        """Vector addition"""
-        return Vec(self.x + other.x, self.y + other.y)
+def rush(rushes, current, slope, forward, back):
+    if current >= slope:
+        return
+    elif current + forward >= slope:
+        rushes[0] += 1
+        return
+    else:
+        rushes[0] += 1
+        rush(rushes, current + forward - back, slope, forward, back)
 
-    def __sub__(self, other):
-        """Vector subtraction"""
-        return Vec(self.x - other.x, self.y - other.y)
-    
-    def __mul__(self, scale):
-        """Multiplication by a scalar"""
-        return Vec(self.x * scale, self.y * scale)
-
-    def dot(self, other):
-        """Dot product"""
-        return self.x * other.x + self.y * other.y
-
-    def lensq(self):
-        """The square of the length"""
-        return self.dot(self)
-        
-    def in_box(self, bottom_left, top_right):
-        """True iff this point (self) lies within or on the
-           boundary of the given rectangular box area"""
-        return bottom_left.x <= self.x <= top_right.x and bottom_left.y <= self.y <= top_right.y
-
-    def __getitem__(self, axis):
-        return self.x if axis == 0 else self.y
-
-    def __repr__(self):
-        """String representation of self"""
-        return "({}, {})".format(self.x, self.y)
-        
-
-class QuadTree:
-    """A QuadTree class for COSC262.
-       Richard Lobb, May 2019
-    """
-    MAX_DEPTH = 20
-    def __init__(self, points, centre, size, depth=0, max_leaf_points=2):
-        self.centre = centre
-        self.size = size
-        points = [p for p in points if 
-        self.centre.x - self.size / 2 <= p.x < self.centre.x + self.size / 2 
-        and self.centre.y - self.size / 2 <= p.y < self.centre.y + self.size / 2]
-        self.depth = depth
-        self.max_leaf_points = max_leaf_points
-        self.points = points
-        self.is_leaf = False
-        self.children = []
-        self.build_children()
-                
-    def build_children(self):
-        print(f"depth={self.depth}, points={self.points}, centre={self.centre}")
-        if len(self.points) > self.max_leaf_points and self.depth < self.MAX_DEPTH:
-            for i in range(4):
-                child_centre = Vec(self.centre.x - self.size/4 + self.size/2 * (i > 1), 
-                self.centre.y - self.size/4 + self.size / 2 * (i % 2))
-                child_size = self.size / 2
-                child = QuadTree(self.points, child_centre, child_size, self.depth + 1, 
-                self.max_leaf_points)
-                self.children.append(child)
-        else:
-            self.is_leaf = True
-
-    def plot(self, axes):
-        """Plot the dividing axes of this node and
-           (recursively) all children"""
-        if self.is_leaf:
-            axes.plot([p.x for p in self.points], [p.y for p in self.points], 'bo')
-        else:
-            axes.plot([self.centre.x - self.size / 2, self.centre.x + self.size / 2],
-                      [self.centre.y, self.centre.y], '-', color='gray')
-            axes.plot([self.centre.x, self.centre.x],
-                      [self.centre.y - self.size / 2, self.centre.y + self.size / 2],
-                      '-', color='gray')
-            for child in self.children:
-                child.plot(axes)
-        axes.set_aspect(1)
-                
-    def __repr__(self, depth=0):
-        """String representation with children indented"""
-        indent = 2 * self.depth * ' '
-        if self.is_leaf:
-            return indent + "Leaf({}, {}, {})".format(self.centre, self.size, self.points)
-        else:
-            s = indent + "Node({}, {}, [\n".format(self.centre, self.size)
-            for child in self.children:
-                s += child.__repr__(depth + 1) + ',\n'
-            s += indent + '])'
-            return s
-        
-import matplotlib.pyplot as plt
-points = [(60, 15), (15, 60), (30, 58), (42, 66), (40, 70)]
-vecs = [Vec(*p) for p in points]
-tree = QuadTree(vecs, Vec(50, 50), 100)
-print(tree)
-
-# Plot the tree, for debugging only
-axes = plt.axes()
-tree.plot(axes)
-axes.set_xlim(0, 100)
-axes.set_ylim(0, 100)
-plt.show()
+ans = num_rushes(100, 15, 7)
+print(ans)
